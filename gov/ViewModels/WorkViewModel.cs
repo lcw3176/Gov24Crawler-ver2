acb254721d9119142423c0268ed030b53fee3b30
joebrooks <lcw3176@naver.com>
2021-07-03 23:57:10 +0900

    리팩토링

--- gov/ViewModels/WorkViewModel.cs
+++ gov/ViewModels/WorkViewModel.cs
@@ -135,16 +135,4 @@ namespace gov.ViewModels
         }
     }
 
-    public static class DispatcherService
-    {
-        public static void Invoke(Action action)
-        {
-            Dispatcher dispatchObject = Application.Current != null ? Application.Current.Dispatcher : null;
-            if (dispatchObject == null || dispatchObject.CheckAccess())
-                action();
-            else
-                dispatchObject.Invoke(action);
-        }
-    }
-
 }
