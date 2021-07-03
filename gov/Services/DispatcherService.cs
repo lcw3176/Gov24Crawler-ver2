06dce1e8a9208285bf74edcaf1a145c0c6e95b88
joebrooks <lcw3176@naver.com>
2021-07-03 23:56:53 +0900

    서비스 추가

---
+++ gov/Services/DispatcherService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace gov.Services
+{
+    public static class DispatcherService
+    {
+        public static void Invoke(Action action)
+        {
+            Dispatcher dispatchObject = Application.Current != null ? Application.Current.Dispatcher : null;
+            if (dispatchObject == null || dispatchObject.CheckAccess())
+                action();
+            else
+                dispatchObject.Invoke(action);
+        }
+    }
+}
