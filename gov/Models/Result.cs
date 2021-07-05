2434c221b6976d603e3b8ef0fb47f28a4812b807
joebrooks <lcw3176@naver.com>
2021-07-05 20:51:37 +0900

    데이터 추가

--- gov/Models/Result.cs
+++ gov/Models/Result.cs
@@ -2,8 +2,9 @@
 {
     public class Result
     {
-        public int index { get; set; }
-        public bool isValueSuccess { get; set; }
+        public string address { get; set; }
+        public bool isAreaSuccess { get; set; }
+        public bool isOwnerSuccess { get; set; }
         public bool isPictureSuccess { get; set; }
         public string duration { get; set; }
     }
